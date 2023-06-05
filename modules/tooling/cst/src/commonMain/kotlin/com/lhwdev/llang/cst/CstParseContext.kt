c5e639e05e0a6c9646b311ef1c4a11ec4c0226fe
lhwdev <lhwdev6@outlook.com>
2023-06-05 14:37:29 +0900

    🚧 Update CstParseContext: graceful handling (2)

--- modules/tooling/cst/src/commonMain/kotlin/com/lhwdev/llang/cst/CstParseContext.kt
+++ modules/tooling/cst/src/commonMain/kotlin/com/lhwdev/llang/cst/CstParseContext.kt
@@ -1,7 +1,7 @@
 package com.lhwdev.llang.cst
 
-import com.lhwdev.llang.cst.util.DiscardException
-import com.lhwdev.llang.token.CstToken
+import com.lhwdev.llang.parsing.util.ParseContext
+import com.lhwdev.llang.token.Token
 import com.lhwdev.llang.token.TokenKind
 
 
@@ -13,38 +13,88 @@ import com.lhwdev.llang.token.TokenKind
  * [CstParseContext] can be originated from raw code source, serialized CST structure, or
  * existing other [CstParseContext] (for cloning).
  */
-interface CstParseContext {
-	/// Token / Cst Output
-	val code: CstMutableCodeSource
+interface CstParseContext : CstLocalContextSource, ParseContext {
+	@RequiresOptIn
+	annotation class InternalApi
 	
-	fun token(kind: TokenKind): CstToken
+	
+	/// Token / Cst Output
+	val code: CstCodeSource
+	
+	fun token(kind: TokenKind): Token
+	
+	
+	// Local Context
+	
+	fun <T> pushNodeLocalContext(key: CstLocalContextKey<T>, value: T)
+	
+	override fun <T> getLocalContext(key: CstLocalContextKey<T>): T
 	
 	
 	/// Node
 	
-	fun <Node : CstNode> beginNode(allowWs: Boolean = true, light: Boolean = false): Node?
+	/**
+	 * **Not guaranteed to be different** cross different node group.
+	 * Can be used to debug behavior.
+	 */
+	@InternalApi
+	val currentNodeGroupId: Long
+		get() = 0
 	
-	fun <T> pushNodeLocalContext(key: CstLocalContextKey<T>, value: T)
+	@InternalApi
+	fun <Node : CstNode> beginNode(): Node?
 	
-	fun <T> getLocalContext(key: CstLocalContextKey<T>): T
+	@InternalApi
+	fun <Node : CstNode> beginDiscardableNode(): Node?
 	
-	fun <Node : CstNode> beginDiscardable(): Node?
+	@InternalApi
+	fun beforeEndNodeDebugHint(nodeGroupId: Long) {
+	}
 	
 	
-	
-	fun resetCurrentDiscardable()
-	
+	@InternalApi
 	fun <Node : CstNode> endNode(node: Node): Node
 	
-	fun discardDiscardable()
+	@InternalApi
+	fun <Node : CstNode> endDiscardableNode(node: Node): Node
+	
+	/**
+	 * Returns node if graceful error handling is available and applicable for [Node].
+	 * `null` otherwise.
+	 */
+	@InternalApi
+	fun <Node : CstNode> endNodeWithError(throwable: Throwable, info: CstNodeInfo<Node>?): Node?
+	
+	/**
+	 * Returns node if graceful error handling is available and applicable for [Node].
+	 * `null` otherwise.
+	 */
+	@InternalApi
+	fun <Node : CstNode> endDiscardableNodeWithError(
+		throwable: Throwable,
+		info: CstNodeInfo<Node>?
+	): Node?
+	
+	
+	fun resetCurrentDiscardableNode()
 }
 
-inline fun <Node : CstNode> CstParseContext.node(
-	allowWs: Boolean = true,
+@OptIn(CstParseContext.InternalApi::class)
+inline fun <reified Node : CstNode> CstParseContext.node(
 	crossinline block: CstNodeFactory<Node>
 ): Node {
 	beginNode<Node>()?.let { return it }
-	return endNode(this.block())
+	val nodeGroupId = currentNodeGroupId
+	return try {
+		val node = try {
+			this.block()
+		} finally {
+			beforeEndNodeDebugHint(nodeGroupId)
+		}
+		endNode(node)
+	} catch(throwable: Throwable) {
+		endNodeWithError(throwable, nodeInfoOf<Node>()) ?: throw throwable
+	}
 }
 
 /**
@@ -52,14 +102,20 @@ inline fun <Node : CstNode> CstParseContext.node(
  * [CstSelect][com.lhwdev.llang.cst.common.util.CstSelect] etc.
  * Useful for branching such as: 'possible patterns: [A, B] or [C, D, E]'.
  */
-inline fun <Node : CstNode> CstParseContext.discardable(
+@OptIn(CstParseContext.InternalApi::class)
+inline fun <reified Node : CstNode> CstParseContext.discardable(
 	crossinline block: CstNodeFactory<Node>
 ): Node? {
-	beginDiscardable<Node>()?.let { return it }
+	beginDiscardableNode<Node>()?.let { return it }
+	val nodeGroupId = currentNodeGroupId
 	return try {
-		endNode(this.block())
-	} catch(e: DiscardException) {
-		discardDiscardable()
-		null
+		val node = try {
+			this.block()
+		} finally {
+			beforeEndNodeDebugHint(nodeGroupId)
+		}
+		endDiscardableNode(node)
+	} catch(throwable: Throwable) {
+		endDiscardableNodeWithError(throwable, nodeInfoOf<Node>())
 	}
 }
